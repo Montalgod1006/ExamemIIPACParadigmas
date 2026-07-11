af9b7c1cbc79588ebfb95fb08ded65351532fdd6
ntjoann12 <novejoanofc@gmail.com>
2026-07-10 19:21:54 -0600

    Corrigiendo errores en SimulationService.cs y agregando el servicio y el database al program.cs

--- CalculadoraAhorrosApi/Program.cs
+++ CalculadoraAhorrosApi/Program.cs
@@ -1,5 +1,13 @@
+using CalculadoraAhorrosApi.Data;
+using CalculadoraAhorrosApi.Services.Simulation;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddTransient<ISimulationService, SimulationService>();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
