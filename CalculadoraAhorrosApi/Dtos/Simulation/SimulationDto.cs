0fd524662ecf2ec8dad0c32aa7b382c677ae6c9d
HectorGonzales09 <hectorgonzales0911@gmail.com>
2026-07-10 21:04:48 -0600

    Mapper Creado

--- CalculadoraAhorrosApi/Dtos/Simulation/SimulationDto.cs
+++ CalculadoraAhorrosApi/Dtos/Simulation/SimulationDto.cs
@@ -2,7 +2,7 @@ namespace CalculadoraAhorrosApi.Dtos.Simulation
 {
     public class SimulationDto
     {   
-         public string Id { get; set; }
+        public string Id { get; set; }
         public decimal InitialAmount { get; set; }
         public decimal InitialTaxYearly { get; set; }
         public int PlazoDeAños { get; set; }
--- CalculadoraAhorrosApi/Services/Simulation/ISimulationService.cs
+++ CalculadoraAhorrosApi/Services/Simulation/ISimulationService.cs
@@ -6,9 +6,9 @@ namespace CalculadoraAhorrosApi.Services.Simulation
     public interface ISimulationService
     {
         Task<ResponseDto<PageDto<List<SimulationDto>>>> GetPageAsync(string searchTerm = "", int page = 1, int pageSize = 10);
-         Task<ResponseDto<SimulationDto>> GetOneByIdAsync(string id);
-         Task<ResponseDto<SimulationDto>> GetOneByIdAsyncMonthly(string id);
-         Task<ResponseDto<SimulationDto>> GetOneByIdAsyncYearly(string id);
+        Task<ResponseDto<SimulationDto>> GetOneByIdAsync(string id);
+        Task<ResponseDto<SimulationDto>> GetOneByIdAsyncMonthly(string id);
+        Task<ResponseDto<SimulationDto>> GetOneByIdAsyncYearly(string id);
         Task <ResponseDto<SimulationActionResponseDto>> CreateAsync(SimulationCreateDto dto);
 
     }
